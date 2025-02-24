4ff49cdaf72f32bcd52715825246a2133532bb64
Douglas-2025 <douglascode2025@gmail.com>
2025-02-24 13:01:21 -0400

    Listagem de produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,58 @@
 
 export default function Home() {
   return (
-   <div className="max">
+   <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        Prod 1
+      </div>
 
-    <h1>Next E-commerce 15.1.7</h1>
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        Prod 1
+      </div>
+
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        Prod 1
+      </div>
+
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        Prod 1
+      </div>
+
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        Prod 1
+      </div>
+
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        Prod 1
+      </div>
+
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        Prod 1
+      </div>
+
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        Prod 1
+      </div>
+
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        Prod 1
+      </div>
+
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        Prod 1
+      </div>
+
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        Prod 1
+      </div>
+
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        Prod 1
+      </div>
+    </div>
+
+    
 
   </div> 
   );
