0f80c8f596fcc2ff7195693e83959be6b32958e8
Douglas-2025 <douglascode2025@gmail.com>
2025-02-24 15:39:28 -0400

    Adicionando autenticação com clark

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,7 @@
+import { SignedIn, SignedOut} from '@clerk/nextjs';
 import Link from 'next/link';
+import { SignInButton, UserButton } from '@clerk/nextjs';
+
 
 function Navbar() {
     return (
@@ -6,6 +9,20 @@ function Navbar() {
         <a href="/" className="uppercase font-bold text-md h-12 flex items-center">
           BLLAPRATA 925
         </a>
+        <div className='flex items-center gap-8'>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+
+          <SignedOut>
+             <SignInButton mode='modal'>
+                <button className='border rounded-md border-gray-400 px-3 py-3'> 
+                  Fazer Login 
+                </button>
+              </SignInButton>
+            </SignedOut>
+
+        </div>
       </nav>
     )
 }
