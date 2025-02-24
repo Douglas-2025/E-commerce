eca7367b63ebfd9acfc1daa95f272c7b3dab4d5d
Douglas-2025 <douglascode2025@gmail.com>
2025-02-24 12:48:12 -0400

    Adicionando navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-   <main>Next E-commerce 15.1.7</main>
+   <div className="max">
+
+    <h1>Next E-commerce 15.1.7</h1>
+
+  </div> 
   );
 }
